4449d9ac8044538e64f95798c3fb21203458708b
Cass Casarez <97998569+cassdoescode@users.noreply.github.com>
2022-06-06 21:31:45 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,118 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+const morgan = require('morgan')
+const PORT = 3001;
+
+app.use(express.json())
+app.use(cors())
+app.use(morgan('tiny'))
+
+app.use(morgan(':object'))
+morgan.token('object', function (req, res) { return `${JSON.stringify(req.body)}`})
+
+let persons = [
+    { 
+        "id": 1,
+        "name": "Arto Hellas", 
+        "number": "040-123456"
+      },
+      { 
+        "id": 2,
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523"
+      },
+      { 
+        "id": 3,
+        "name": "Dan Abramov", 
+        "number": "12-43-234345"
+      },
+      { 
+        "id": 4,
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122"
+      }
+]
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/api/persons/:name', (req, res) => {
+    const name = req.params.name.toLowerCase()
+    if (persons[name]) {
+        res.json(persons[name])
+    } else {
+        res.status(404).json({
+            error: "Does not compute"
+        })
+    }
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    const entry = persons.find(entry => entry.id ==id)
+
+    if(entry){
+        res.json(entry)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.get('/info', (req, res) => {
+    const currentDate = new Date()
+    res.send(`<h2>Phonebook has info for ${persons.length} people</h2> <h2>${currentDate}</h2>`)
+})
+
+const generateID = () => {
+    const maxID = persons.length > 0 
+    ? Math.random(...persons.map(n => n.id))
+    : 0
+    return maxID + 1
+}
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body;
+    if(!body.name) {
+        return res.status(400).json({
+            error: 'Please enter a name.'
+        });
+    }
+
+    if(!body.number) {
+        return res.status(400).json({
+            error: 'Please enter a number.'
+        });
+    }
+
+    if(persons.find(person => person.name === body.name)) {
+        return res.status(400).json({
+            error: 'Duplicate. Please enter a unique name.'
+        })
+    }
+    
+    let person = {
+        id: generateID(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person);
+    res.json(person);
+
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+    id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+    res.status(204).end()
+})
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Now running on port ${PORT}`)
+})
